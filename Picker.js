133d89b62515d4da2f682c886b77a558d8e35dbb
Doug Carter <dougc85@gmail.com>
2021-06-15 13:22:54 -0700

    finished Picker

---
+++ Picker.js
@@ -0,0 +1,70 @@
+class Picker {
+  constructor(newList, medList, knowList, fullKnow) {
+    self.newList = newList;
+    self.medList = medList;
+    self.knowList = knowList;
+    self.fullKnow = fullKnow;
+
+    self.currentList = self.newList;
+    self.choices = ['new', 'new', 'new', 'new', 'med', 'med', 'know'];
+  }
+
+  //Sets self.currentList to the list that will be selected from
+  _chooseList() {
+
+    //If new lists are depleted, always return knowList
+    if (self.newList.length == 0 && self.medList.length == 0) {
+      if (self.knowList.length == 0) {
+        self._resetKnowList();
+      }
+      self.currentList = knowList;
+      return;
+    }
+
+    //Reset choices if necessary
+    if (self.choices.length == 0) {
+      self.choices = ['new', 'new', 'new', 'new', 'med', 'med', 'know'];
+    }
+
+    const choicePosition = Math.floor(Math.rand * self.choices.length)
+    const choice = self.choices[choicePosition];
+    self.choices.splice(choicePosition, 1);
+
+    switch (choice) {
+      case 'new':
+        if (newList.length == 0) {
+          self.chooseList();
+          break;
+        }
+        self.currentList = self.newList;
+        break;
+      case 'med':
+        if (medList.length == 0) {
+          self.chooseList();
+          break;
+        }
+        self.currentList = self.medList;
+        break;
+      default:
+        if (self.knowList.length == 0) {
+          self._resetKnowList();
+        }
+        self.currentList = self.knowList;
+    }
+  }
+
+  _resetKnowList() {
+    self.knowList = [...self.fullKnow];
+  }
+
+  pickTune() {
+
+    self._chooseList();
+
+    const choicePosition = Math.floor(Math.random() * self.currentList.length);
+    const choice = self.currentList[choicePosition];
+    self.currentList.splice(choicePosition, 1);
+
+    return choice;
+  }
+}
