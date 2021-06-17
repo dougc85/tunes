d10d7070e0311c53a994401dba45a5104af0c297
Doug Carter <dougc85@gmail.com>
2021-06-16 23:58:27 -0700

    v 1.1 complete w/ all functionality and styles

--- javascript/data.js
+++ javascript/data.js
@@ -3,16 +3,18 @@ const fullNew = ['Four', 'You don’t know what love is', 'Sweet and Lovely', 'S
 const fullMed = [];
 const fullKnow = ['Half Nelson', 'Embraceable You', 'September in the Rain', 'How Insensitive', 'I Hear a Rhapsody', 'Ceora', 'I Could Write a Book', 'Girl From Ipanema', 'Bolivia', 'Pennies From Heaven', 'Airegin', 'Nearness of You', 'Sleeping Bee', 'Come Fly With Me', 'Along Came Betty (w ending)', 'Come Rain Or Come Shine - Blakey', 'Cheek to Cheek', 'Change Partners', 'You’re Everything', 'Lady is a Tramp', 'Speak No Evil', 'Jitterbug Waltz', 'Joy Spring', 'Detour Ahead', 'I Let Song Go Out My Heart', 'Its Only a Paper Moon (w intro)', 'Darn That Dream', 'Hot House', 'I’ve got the world on a string', 'Yesterdays', 'Here’s that rainy Day', 'April in Paris w/ ending', 'Sweet Georgia Brown', 'Cheryl', 'Unforgettable', 'Almost Like Being in Love', 'I Mean You', 'I Thought About You', 'Moonlight in Vermont', 'Confirmation', 'Mean to Me', 'Well You Needn’t', 'My Foolish Heart', 'Con Alma', 'Body and Soul', 'Up Jumped Spring', 'It Don’t Mean a Thing', 'Prelude to a Kiss', 'You’d be so nice', 'Just Friends', 'Emily', 'Skylark', 'How Deep is the Ocean', 'Windows', 'Bloomdido', 'Blame it on my Youth', 'My Romance', 'Over the Rainbow', 'Jody Grind', 'Young at Heart', 'All or Nothing at all', 'Back at the Chicken Shack', 'Our Love is Here to Stay', 'No more blues', 'Wise One', 'On the Sunny Side of the Street', 'Sandu', 'Dat Dere', 'Donna Lee', 'Doxy', 'When it Rains', 'Night Train', 'Jeannine', 'Gloria’s Step', 'Whisper Not', 'When Sunny Gets Blue', 'Witchcraft', 'Black Nile', 'I’ve got you under my skin', 'Inner Urge', 'Stardust', 'Blue Train', '26-2', 'There is No Greater Love', 'Nica’s Dream', 'Isfahan', 'Cool Eyes', 'Yancey Special', 'L-o-v-e', 'Milestones', 'Moose the Mooche', 'The Very Thought if You', 'In Your Own Sweet Way', 'How about you', 'Recorda-Me', 'Ask Me Now', 'Yardbird suite', 'Round Midnight (intro &coda)', 'That Old Black Magic', 'Fee Fi Fo Fum', 'Samba de Orfeo', 'UMMG', 'Ornithology', 'Sleeping Dancer Sleep On', 'Dance of the Infidels', 'Pensativa', 'Conception', 'It never entered my mind', 'You make me feel so young', 'Moment’s Notice', 'Deed I do', 'Blues for Alice', 'Dindi W intro', 'Bye-Ya', 'Bouncing with Bud', 'Syeeda’s Song Flute', 'Sophisticated Lady', 'Valse Hot', 'Love For Sale', 'Wail', 'Sugar Ray'];
 
-
 const tuneStorageNewJSON = window.localStorage.getItem('tuneStorageNew');
 const tuneStorageMedJSON = window.localStorage.getItem('tuneStorageMed');
 const tuneStorageKnowJSON = window.localStorage.getItem('tuneStorageKnow');
+const tuneStorageEditsJSON = window.localStorage.getItem('tuneStorageEdits');
 
 let newList = tuneStorageNewJSON ? (JSON.parse(tuneStorageNewJSON)).new : [];
 let medList = tuneStorageMedJSON ? (JSON.parse(tuneStorageMedJSON)).med : [];
 let knowList = tuneStorageKnowJSON ? (JSON.parse(tuneStorageKnowJSON)).know : [];
+let editsArray = tuneStorageEditsJSON ? (JSON.parse(tuneStorageEditsJSON)).edits : [];
 
-// Store lists in storage and then do that in the Picker file, too
+
+
 if (newList == undefined || newList.length == 0) {
   newList = [...fullNew];
 }
@@ -22,7 +24,11 @@ if (medList == undefined || medList.length == 0) {
 if (knowList == undefined || knowList.length == 0) {
   knowList = [...fullKnow];
 }
+if (editsArray == undefined) {
+  editsArray = [];
+}
 
 window.localStorage.setItem('tuneStorageNew', JSON.stringify({ new: newList }));
 window.localStorage.setItem('tuneStorageMed', JSON.stringify({ med: medList }));
 window.localStorage.setItem('tuneStorageKnow', JSON.stringify({ know: knowList }));
+window.localStorage.setItem('tuneStorageEdits', JSON.stringify({ edits: editsArray }));
--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,11 @@
+//Global Vars
 const picker = new Picker(newList, medList, knowList, fullKnow);
+let currentList = '';
+let currentTune = '';
 
+let targetList = '';
+
+//DOM variables
 const body = document.querySelector('body');
 
 const tuneName = document.querySelector('.tune-name');
@@ -10,11 +16,28 @@ const skipButton = document.querySelector('.skip-button');
 const raiseButton = document.querySelector('.raise-button');
 const lowerButton = document.querySelector('.lower-button');
 
+const editsBackground = document.querySelector('.edits-background');
+const edits = document.querySelector('.edits');
+const seeEditsButton = document.querySelector('.see-edits-button');
+const editsClose = document.querySelector('.edits-close');
+const editsList = document.querySelector('.edits-list');
+
+const levelChangePopup = document.querySelector('.level-change-popup');
+const popupBackground = document.querySelector('.popup-background');
+const warning = document.querySelector('.warning');
+const cancelButton = document.querySelector('.cancel');
+const confirmButton = document.querySelector('.confirm');
+const okayButton = document.querySelector('.okay');
+let clearButton = document.querySelector('.clear');
+
 function nextHandler() {
 
   const newList = picker.chooseList();
   const newTune = picker.pickTune();
   const newKey = picker.pickKey();
+
+  currentList = newList;
+  currentTune = newTune;
 
   if (newTune.length > 20) {
     tuneName.style.fontSize = "4rem";
@@ -32,13 +55,13 @@ function nextHandler() {
 
   switch (newList) {
     case 'new':
-      body.style.backgroundColor = '#f1c40f';
+      body.style.backgroundColor = 'hsl(26, 100%, 67%)';
       break;
     case 'med':
-      body.style.backgroundColor = '#f4a322';
+      body.style.backgroundColor = 'hsl(54, 98%, 66%)';
       break;
     case 'know':
-      body.style.backgroundColor = '#2ecc71';
+      body.style.backgroundColor = 'hsl(145, 63%, 50%)';
       break;
   }
 }
@@ -55,7 +78,157 @@ function skipHandler() {
   }
 }
 
+function seeEditsHandler() {
+  editsBackground.style.height = '100vh';
+  editsBackground.style.opacity = '1';
+
+  edits.classList.add('edits-onclick');
+}
+
+function editsCloseHandler() {
+  edits.classList.remove('edits-onclick');
+  setTimeout(() => {
+    editsBackground.style.opacity = '0';
+  }, 150);
+  setTimeout(() => {
+    editsBackground.style.height = '0vh';
+  }, 400);
+}
+
+function raiseHandler() {
+  levelChangePopup.classList.add('popped-popup');
+  popupBackground.style.height = ('105vh');
+  popupBackground.style.opacity = ('1');
+
+
+  if (currentList == 'new') {
+    levelChangePopup.style.backgroundColor = "hsl(54, 98%, 66%)";
+    warning.textContent = 'Are you sure you want to move this song up to the "medium" level?'
+    targetList = 'med';
+
+  } else if (currentList == 'med') {
+    levelChangePopup.style.backgroundColor = "hsl(145, 63%, 50%)";
+    warning.textContent = 'Are you sure you want to move this song up to the "know" level?'
+    targetList = 'know';
+
+  } else if (currentList == 'know') {
+    levelChangePopup.style.backgroundColor = "#eee";
+
+    okayButton.style.display = 'inline-block';
+    confirmButton.style.display = 'none';
+    cancelButton.style.display = 'none';
+
+    warning.textContent = 'Already at highest level'
+  }
+}
+
+function lowerHandler() {
+  levelChangePopup.classList.add('popped-popup');
+  popupBackground.style.height = ('105vh');
+  popupBackground.style.opacity = ('1');
+
+  if (currentList == 'new') {
+    levelChangePopup.style.backgroundColor = "#eee";
+    warning.textContent = 'Already at lowest level';
+
+    okayButton.style.display = 'inline-block';
+    confirmButton.style.display = 'none';
+    cancelButton.style.display = 'none';
+
+  } else if (currentList == 'med') {
+    levelChangePopup.style.backgroundColor = "hsl(26, 100%, 67%)";
+    warning.textContent = 'Are you sure you want to move this song down to lowest level?';
+    targetList = 'new';
+
+
+  } else if (currentList == 'know') {
+    levelChangePopup.style.backgroundColor = "hsl(26, 100%, 67%)";
+    warning.textContent = 'Are you sure you want to move this song down to lowest level?';
+    targetList = 'new';
+  }
+}
+
+function removePopup() {
+  levelChangePopup.classList.remove('popped-popup');
+  setTimeout(() => {
+    popupBackground.style.opacity = '0';
+  }, 150);
+  setTimeout(() => {
+    popupBackground.style.height = '0vh';
+  }, 400);
+}
+
+function cancelHandler() {
+
+  removePopup();
+
+}
+
+function okayHandler() {
+
+  removePopup();
+
+  setTimeout(() => {
+    okayButton.style.display = 'none';
+    confirmButton.style.display = 'inline-block';
+    cancelButton.style.display = 'inline-block';
+  }, 400);
+}
+
+function confirmHandler() {
+
+  removePopup();
+
+  editsArray.push(`Add "${currentTune}" to ${targetList}`);
+  renderEdits();
+  window.localStorage.setItem('tuneStorageEdits', JSON.stringify({ edits: editsArray }));
+}
+
+function createClearButton() {
+  let clearContainer = document.createElement('div');
+  clearContainer.classList.add('clear-container');
+  let clearButtonNew = document.createElement('button');
+  clearButtonNew.classList.add('clear');
+  clearButtonNew.textContent = 'CLEAR';
+  clearContainer.append(clearButtonNew);
+  editsList.append(clearContainer);
+  clearButton = clearButtonNew;
+  clearButton.addEventListener('click', clearHandler);
+}
+
+function renderEdits() {
+
+  editsList.innerHTML = '';
+
+  for (let i = 0; i < editsArray.length; i++) {
+    const newLi = document.createElement('li');
+    newLi.classList.add('edits-list-item')
+    newLi.textContent = editsArray[i];
+    editsList.append(newLi);
+  }
+
+  createClearButton();
+}
+
+function clearHandler() {
+  editsList.innerHTML = '';
+  editsArray = [];
+  window.localStorage.setItem('tuneStorageEdits', JSON.stringify({ edits: editsArray }));
+
+  createClearButton();
+}
+
+
 nextButton.addEventListener('click', nextHandler);
 skipButton.addEventListener('click', skipHandler);
+seeEditsButton.addEventListener('click', seeEditsHandler);
+editsClose.addEventListener('click', editsCloseHandler);
+raiseButton.addEventListener('click', raiseHandler);
+lowerButton.addEventListener('click', lowerHandler);
+cancelButton.addEventListener('click', cancelHandler);
+confirmButton.addEventListener('click', confirmHandler);
+okayButton.addEventListener('click', okayHandler);
+clearButton.addEventListener('click', clearHandler);
 
+renderEdits();
 nextHandler();
